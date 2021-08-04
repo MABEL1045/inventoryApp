d03cd3ba3166d52e29229cdc5142bb5f14e2a433
Back-end Class A <back-endclassa@DSIs-iMac.lan>
2021-08-04 09:49:49 +0100

    commit

--- app.js
+++ app.js
@@ -8,20 +8,19 @@ const PORT = 5000;
 const Db = mongoose.connect("mongodb+srv://mabelDB:mabel6723@cluster0.k29nd.mongodb.net/inventory2?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
-    console.log("connected to db"),
-      app.listen(PORT, () => console.log(`server listening on ${PORT}....`));
-  }
-);
+    console.log("connected to db")
+  })
+    
+    // Import routes
+    const productRoute = require("./src/routes/productRoute");
+    const userRoute = require("./src/routes/userRoute");
+    
+    // // Parse json
+    app.use(express.json());
+    
+    // // Set routes
+    app.use("/api/products", productRoute);
+    app.use("/api/user", userRoute);
+    
+    app.listen(PORT, () => console.log(`server listening on ${PORT}....`));
 
-// Import routes
-const productRoute = require("./src/routes/productRoute");
-const userRoute = require("./src/routes/userRoute");
-
-// // Parse json
-app.use(express.json());
-
-// // Set routes
-app.use("/api/products", productRoute);
-app.use("/api/user", userRoute);
-
-console.log("Waiting for database...");
